f14c6a7004d0ae7eec2625de84173388d696e4b3
Mara Timberlake <maratimberlake@msn.com>
2017-09-06 23:14:59 -0700

    create click listener

--- app/src/main/java/com/mara/myresturants/MainActivity.java
+++ app/src/main/java/com/mara/myresturants/MainActivity.java
@@ -1,8 +1,17 @@
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
