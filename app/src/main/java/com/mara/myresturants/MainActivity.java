95282450820b8d14da72403865a8695d04c4b6d2
Mara Timberlake <maratimberlake@msn.com>
2017-09-06 23:17:23 -0700

    edit mFindRestaurantsButton to navigates to new activity when clicked

--- app/src/main/java/com/mara/myresturants/MainActivity.java
+++ app/src/main/java/com/mara/myresturants/MainActivity.java
@@ -1,3 +1,11 @@
+package com.epicodus.myrestaurants;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
 
@@ -5,12 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
             }
         });
     }
