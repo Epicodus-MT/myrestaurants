cefc686f989ebf68823cf4d1144e4f719944b52f
Mara Timberlake <maratimberlake@msn.com>
2017-09-25 13:13:40 -0700

    implement the drag-and-drop feature

--- app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantViewHolder.java
@@ -1,26 +1,26 @@
 package com.epicodus.myrestaurants.adapters;
 
-        import android.content.Context;
-        import android.content.Intent;
-        import android.support.v7.widget.RecyclerView;
-        import android.view.View;
-        import android.widget.ImageView;
-        import android.widget.TextView;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-        import com.epicodus.myrestaurants.Constants;
-        import com.epicodus.myrestaurants.R;
-        import com.epicodus.myrestaurants.models.Restaurant;
-        import com.epicodus.myrestaurants.ui.RestaurantDetailActivity;
-        import com.google.firebase.database.DataSnapshot;
-        import com.google.firebase.database.DatabaseError;
-        import com.google.firebase.database.DatabaseReference;
-        import com.google.firebase.database.FirebaseDatabase;
-        import com.google.firebase.database.ValueEventListener;
-        import com.squareup.picasso.Picasso;
+import com.epicodus.myrestaurants.Constants;
+import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.ui.RestaurantDetailActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
-        import org.parceler.Parcels;
+import org.parceler.Parcels;
 
-        import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private static final int MAX_WIDTH = 200;
@@ -28,6 +28,8 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
 
     View mView;
     Context mContext;
+    public ImageView mRestaurantImageView;
+
 
     public FirebaseRestaurantViewHolder(View itemView) {
         super(itemView);
@@ -37,7 +39,7 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
     }
 
     public void bindRestaurant(Restaurant restaurant) {
-        ImageView restaurantImageView = (ImageView) mView.findViewById(R.id.restaurantImageView);
+        mRestaurantImageView = (ImageView) mView.findViewById(R.id.restaurantImageView);
         TextView nameTextView = (TextView) mView.findViewById(R.id.restaurantNameTextView);
         TextView categoryTextView = (TextView) mView.findViewById(R.id.categoryTextView);
         TextView ratingTextView = (TextView) mView.findViewById(R.id.ratingTextView);
@@ -46,7 +48,7 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
                 .load(restaurant.getImageUrl())
                 .resize(MAX_WIDTH, MAX_HEIGHT)
                 .centerCrop()
-                .into(restaurantImageView);
+                .into(mRestaurantImageView);
 
         nameTextView.setText(restaurant.getName());
         categoryTextView.setText(restaurant.getCategories().get(0));
