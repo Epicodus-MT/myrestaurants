0a17f9b0732f41a15cf0efacd5126c796924c00d
Mara Timberlake <maratimberlake@msn.com>
2017-09-25 12:11:34 -0700

    replace 'restaurant_list_item' with 'restaurant_list_item_drag'; currently icons appear over ImageViews

--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -1,22 +1,22 @@
 package com.epicodus.myrestaurants.ui;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.support.v7.widget.LinearLayoutManager;
-        import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-        import com.epicodus.myrestaurants.Constants;
-        import com.epicodus.myrestaurants.R;
-        import com.epicodus.myrestaurants.adapters.FirebaseRestaurantViewHolder;
-        import com.epicodus.myrestaurants.models.Restaurant;
-        import com.firebase.ui.database.FirebaseRecyclerAdapter;
-        import com.google.firebase.auth.FirebaseAuth;
-        import com.google.firebase.auth.FirebaseUser;
-        import com.google.firebase.database.DatabaseReference;
-        import com.google.firebase.database.FirebaseDatabase;
+import com.epicodus.myrestaurants.Constants;
+import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.adapters.FirebaseRestaurantViewHolder;
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-        import butterknife.Bind;
-        import butterknife.ButterKnife;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SavedRestaurantListActivity extends AppCompatActivity {
     private DatabaseReference mRestaurantReference;
@@ -34,14 +34,17 @@ public class SavedRestaurantListActivity extends AppCompatActivity {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = user.getUid();
 
+        mRestaurantReference = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_RESTAURANTS)
+                .child(uid);
 
-        mRestaurantReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS).child(uid);
         setUpFirebaseAdapter();
     }
 
     private void setUpFirebaseAdapter() {
         mFirebaseAdapter = new FirebaseRecyclerAdapter<Restaurant, FirebaseRestaurantViewHolder>
-                (Restaurant.class, R.layout.restaurant_list_item, FirebaseRestaurantViewHolder.class,
+                (Restaurant.class, R.layout.restaurant_list_item_drag, FirebaseRestaurantViewHolder.class,
                         mRestaurantReference) {
 
             @Override
