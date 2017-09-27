a21e92ccbc213632200f249fe405cb8eccdd4089
Mara Timberlake <maratimberlake@msn.com>
2017-09-27 16:05:33 -0700

    fetch extra info included in intent

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantDetailActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantDetailActivity.java
@@ -1,38 +1,67 @@
 package com.epicodus.myrestaurants.ui;
 
-import android.support.v4.view.ViewPager;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
-import com.epicodus.myrestaurants.adapters.RestaurantPagerAdapter;
 import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.util.OnRestaurantSelectedListener;
 
 import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
-public class RestaurantDetailActivity extends AppCompatActivity {
-    @Bind(R.id.viewPager) ViewPager mViewPager;
-    private RestaurantPagerAdapter adapterViewPager;
-    ArrayList<Restaurant> mRestaurants = new ArrayList<>();
-
+public class RestaurantListActivity extends AppCompatActivity implements OnRestaurantSelectedListener {
+    private Integer mPosition;
+    ArrayList<Restaurant> mRestaurants;
+    String mSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant_detail);
-        ButterKnife.bind(this);
+        setContentView(R.layout.activity_restaurants);
 
-        mRestaurants = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+        if (savedInstanceState != null) {
 
-        int startingPosition = getIntent().getIntExtra("position", 0);
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                mPosition = savedInstanceState.getInt(Constants.EXTRA_KEY_POSITION);
+                mRestaurants = Parcels.unwrap(savedInstanceState.getParcelable(Constants.EXTRA_KEY_RESTAURANTS));
+                mSource = savedInstanceState.getString(Constants.KEY_SOURCE);
 
-        adapterViewPager = new RestaurantPagerAdapter(getSupportFragmentManager(), mRestaurants);
-        mViewPager.setAdapter(adapterViewPager);
-        mViewPager.setCurrentItem(startingPosition);
+                if (mPosition != null && mRestaurants != null) {
+                    Intent intent = new Intent(this, RestaurantDetailActivity.class);
+                    intent.putExtra(Constants.EXTRA_KEY_POSITION, mPosition);
+                    intent.putExtra(Constants.EXTRA_KEY_RESTAURANTS, Parcels.wrap(mRestaurants));
+                    intent.putExtra(Constants.KEY_SOURCE, mSource);
+                    startActivity(intent);
+                }
+
+            }
+
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (mPosition != null && mRestaurants != null) {
+            outState.putInt(Constants.EXTRA_KEY_POSITION, mPosition);
+            outState.putParcelable(Constants.EXTRA_KEY_RESTAURANTS, Parcels.wrap(mRestaurants));
+            outState.putString(Constants.KEY_SOURCE, mSource);
+        }
+
+    }
+
+    @Override
+    public void onRestaurantSelected(Integer position, ArrayList<Restaurant> restaurants, String source) {
+        mPosition = position;
+        mRestaurants = restaurants;
+        mSource = source;
     }
 }
